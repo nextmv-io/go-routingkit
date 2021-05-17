10e311808e5cdcaa799433065d31c3905b5cac56
Marius Merschformann <marius.merschformann@gmail.com>
2021-05-18 01:44:29 +0200

    Adding file based testing

--- cmd/routingkit/main.go
+++ cmd/routingkit/main.go
@@ -2,7 +2,10 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"time"
 
@@ -16,6 +19,8 @@ func main() {
 
 	myClass := routingkit.NewClient()
 	defer routingkit.DeleteClient(myClass)
+
+	testFile(myClass)
 
 	start := time.Now()
 	if _, err := os.Stat(chFile); os.IsNotExist(err) {
@@ -62,4 +67,61 @@ func main() {
 	// }
 	// wg.Wait()
 	//fmt.Println("100 queries took %v", time.Since(start))
+}
+
+func testFile(client routingkit.Client) {
+	// Prepare test data
+	var file string
+	flag.StringVar(
+		&file,
+		"testfile",
+		"",
+		"path to test file",
+	)
+	flag.Parse()
+	if file == "" {
+		fmt.Println("Skipping file test (no file specified; use -testfile <path>)")
+		return
+	}
+	fmt.Println("Reading points from file: ", file)
+	points := readPoints(file)
+
+	// Execute test query
+	start := time.Now()
+	pointVector := routingkit.NewPointVector(int64(len(points)))
+	defer routingkit.DeletePointVector(pointVector)
+	for i, point := range points {
+		p := routingkit.NewPoint()
+		p.SetLon(float32(point[0]))
+		p.SetLat(float32(point[1]))
+		pointVector.Set(i, p)
+	}
+	distances := client.Table(pointVector, pointVector)
+	queries := len(points) * len(points)
+	elapsed := time.Since(start)
+	avg := time.Duration(float64(elapsed.Nanoseconds())/float64(queries)) * time.Nanosecond
+	fmt.Printf("%d queries took %v (avg: %v)\n", queries, elapsed, avg)
+	var total float32
+	for i := 0; i < int(distances.Size()); i++ {
+		total += distances.Get(i)
+	}
+	fmt.Printf("avg. distance: %f\n", total/float32(distances.Size()))
+}
+
+func readPoints(path string) [][2]float64 {
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	var b pointData
+	err = json.Unmarshal(dat, &b)
+	if err != nil {
+		fmt.Printf("error deserializing: %s\n", string(dat))
+		return [][2]float64{}
+	}
+	return b.Points
+}
+
+type pointData struct {
+	Points [][2]float64 `json:"points"`
 }
