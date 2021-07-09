f55722a70bbc5551b675878c30a447162e6d4f00
Nils Beckmann <>
2021-07-09 18:28:42 +0200

    generate apple silicon build

--- third_party/setup.sh
+++ third_party/setup.sh
@@ -16,10 +16,10 @@ esac
 git clone https://github.com/RoutingKit/RoutingKit.git || (cd RoutingKit ; git pull; cd ..)
 cd RoutingKit
 if [ "$GOOS" = "darwin" ]; then
-	sed -i 's/CC=g++/CC=clang++/' Makefile
-	sed -i 's/\(CFLAGS=.*-std=c++11\) \(.*\)/\1 -stdlib=libc++ \2/' Makefile
-	sed -i 's/OMP_CFLAGS=-fopenmp/OMP_CFLAGS=-Xpreprocessor -fopenmp -lomp/' Makefile
-	sed -i 's/OMP_LDFLAGS=-fopenmp/OMP_LDFLAGS=-Xpreprocessor -fopenmp -lomp/' Makefile
+	sed -i '' "s/CC=g++/CC=clang++/" Makefile
+	sed -i '' "s/\(CFLAGS=.*-std=c++11\) \(.*\)/\1 -stdlib=libc++ \2/" Makefile
+	sed -i '' "s/OMP_CFLAGS=-fopenmp/OMP_CFLAGS=-Xpreprocessor -fopenmp -lomp/" Makefile
+	sed -i '' "s/OMP_LDFLAGS=-fopenmp/OMP_LDFLAGS=-Xpreprocessor -fopenmp -lomp/" Makefile
 
 	if [ "$GOARCH" = "arm64" ]; then
 		sed -i '' "s/-march=native/-mcpu=apple-a14/" Makefile
