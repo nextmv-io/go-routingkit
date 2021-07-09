3833db6d51b03b6bc9ee454e2746b852362b8540
brandon <brandon@nextmv.io>
2021-07-09 08:27:26 -0700

    compile for a14 in makefile

--- third_party/setup.sh
+++ third_party/setup.sh
@@ -3,12 +3,14 @@ set -eu
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd "${HERE}" || exit 1
 GOOS="$( go env GOOS )"
+GOARCH="$( go env GOARCH )"
 case $GOOS in
 	linux)
 		sudo apt-get install -y zlib1g-dev
 	;;
 	darwin)
-		sudo brew install zlib
+		brew install zlib
+		brew install libomp
 	;;
 esac
 git clone https://github.com/RoutingKit/RoutingKit.git || (cd RoutingKit ; git pull; cd ..)
@@ -18,7 +20,12 @@ if [ "$GOOS" = "darwin" ]; then
 	sed -i 's/\(CFLAGS=.*-std=c++11\) \(.*\)/\1 -stdlib=libc++ \2/' Makefile
 	sed -i 's/OMP_CFLAGS=-fopenmp/OMP_CFLAGS=-Xpreprocessor -fopenmp -lomp/' Makefile
 	sed -i 's/OMP_LDFLAGS=-fopenmp/OMP_LDFLAGS=-Xpreprocessor -fopenmp -lomp/' Makefile
+
+	if [ "$GOARCH" = "arm64" ]; then
+		sed -i '' "s/-march=native/-mcpu=apple-a14/" Makefile
+	fi
 fi
+rm -rv build
 make
 rm -v lib/libroutingkit.so
 popd
